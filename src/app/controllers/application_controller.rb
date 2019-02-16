92493f5f9a6a0e886ab36bac7d53754f55a60118
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-02-16 21:06:32 +0900

    ログアウトの実装

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 	protect_from_forgery with: :exception
+
+	def after_sign_out_path_for(resource)
+		new_user_session_path
+	end
 end
