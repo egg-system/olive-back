c01467fb5cd809c5343f3201be4b484335ef556b
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-03-12 20:37:13 +0900

    migrationの修正

--- src/db/migrate/20190116151135_create_shifts.rb
+++ src/db/migrate/20190116151135_create_shifts.rb
@@ -1,23 +1,21 @@
 class CreateShifts < ActiveRecord::Migration[5.1]
   def change
     create_table :shifts do |t|
-      t.integer :shift_id
       t.date :shift_date
-      t.integer :store_id
-      t.integer :staff_id
-      t.time :shift_begin
-      t.time :shift_end
-      t.boolean :time_1011
-      t.boolean :time_1011
-      t.boolean :time_1112
-      t.boolean :time_1213
-      t.boolean :time_1314
-      t.boolean :time_1415
-      t.boolean :time_1516
-      t.boolean :time_1617
-      t.boolean :time_1718
-      t.boolean :time_1819
-      t.boolean :time_1920
+      t.references :store, foreign_key: true
+      t.references :staff, foreign_key: true
+      t.time :start_at
+      t.time :end_at
+      t.boolean :on_shift_10_to_11, comment: '10時~11時に施術可能か'
+      t.boolean :on_shift_11_to_12, comment: '11時~12時に施術可能か'
+      t.boolean :on_shift_12_to_13, comment: '12時~13時に施術可能か'
+      t.boolean :on_shift_13_to_14, comment: '13時~14時に施術可能か'
+      t.boolean :on_shift_14_to_15, comment: '14時~15時に施術可能か'
+      t.boolean :on_shift_15_to_16, comment: '15時~16時に施術可能か'
+      t.boolean :on_shift_16_to_17, comment: '16時~17時に施術可能か'
+      t.boolean :on_shift_17_to_18, comment: '17時~18時に施術可能か'
+      t.boolean :on_shift_18_to_19, comment: '18時~19時に施術可能か'
+      t.boolean :on_shift_19_to_20, comment: '19時~20時に施術可能か'
 
       t.timestamps
     end
