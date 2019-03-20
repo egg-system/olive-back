8855713467a6da79c93bee8654dc320478ff5c0b
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-03-20 21:58:38 +0900

    transactionの追加

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,17 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')) do |file|
-  load(file)
+# 本番データ投入用のseeder
+ActiveRecord::Base.transaction do
+  Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')) do |file|
+    load(file)
+  end
 end
 
-unless Rails.env.production?
-  Dir.glob(File.join(Rails.root, 'db', 'seeds', 'test', '*.rb')) do |file|
-    load(file)
+# テストデータ用のseeder
+ActiveRecord::Base.transaction do
+  unless Rails.env.production?
+    Dir.glob(File.join(Rails.root, 'db', 'seeds', 'test', '*.rb')) do |file|
+      load(file)
+    end
   end
 end
