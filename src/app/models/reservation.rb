d30838d30a5be548edd79dc3a018c02eee9e9c88
koheikameyama <kohei.kameyama@react.tokyo>
2019-03-22 15:12:49 +0900

    顧客管理画面作成

--- src/app/models/reservation.rb
+++ src/app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
     belongs_to :pregnant_status
     belongs_to :with_child_status
+
+    scope :join_staff, ->{
+        left_joins(:staff).select("reservations.*, staffs.name as staff_name")
+    }
 end
