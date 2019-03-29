1367eca9847eec5449f713fbd1ea715ad05fd618
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-03-29 13:35:56 +0900

    apiの処理を追加

--- src/app/models/store.rb
+++ src/app/models/store.rb
@@ -1,8 +1,22 @@
 class Store < ApplicationRecord
   enum store_type: { owned: 0, franchised: 1 }
 
+  has_many :store_menu
+  has_many :menus, through: :store_menu
+
   def to_shop
     shop_attributes
+  end
+
+  def to_shop_menus
+    shop_menus = [ sub_shop_attributes ]
+    
+    if is_head
+      este_store = Store.find(Settings.stores.head_este_store_id)
+      shop_menus.concat(este_store.to_shop_menus)
+    end
+
+    return shop_menus
   end
 
   private
@@ -12,5 +26,13 @@ class Store < ApplicationRecord
     SHOP_KEYS.map { |json_key|
       [json_key, attributes[json_key.to_s]]
     }.to_h.to_json
+  end
+
+  def is_head
+    self.id === Settings.stores.head_store_id
+  end
+
+  def sub_shop_attributes
+    { id: self.id, name: self.name, menus: self.menus }
   end
 end
