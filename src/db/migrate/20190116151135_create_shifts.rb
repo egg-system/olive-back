5d0cd68a03250d2ae1a1255f8fcb837e76d76ca5
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-04-09 00:21:28 +0900

    fix : default値の追加

--- src/db/migrate/20190116151135_create_shifts.rb
+++ src/db/migrate/20190116151135_create_shifts.rb
@@ -4,16 +4,16 @@ class CreateShifts < ActiveRecord::Migration[5.1]
       t.date :date
       t.references :store, foreign_key: true
       t.references :staff, foreign_key: true
-      t.boolean :on_shift_10_to_11, comment: '10時~11時に施術可能か'
-      t.boolean :on_shift_11_to_12, comment: '11時~12時に施術可能か'
-      t.boolean :on_shift_12_to_13, comment: '12時~13時に施術可能か'
-      t.boolean :on_shift_13_to_14, comment: '13時~14時に施術可能か'
-      t.boolean :on_shift_14_to_15, comment: '14時~15時に施術可能か'
-      t.boolean :on_shift_15_to_16, comment: '15時~16時に施術可能か'
-      t.boolean :on_shift_16_to_17, comment: '16時~17時に施術可能か'
-      t.boolean :on_shift_17_to_18, comment: '17時~18時に施術可能か'
-      t.boolean :on_shift_18_to_19, comment: '18時~19時に施術可能か'
-      t.boolean :on_shift_19_to_20, comment: '19時~20時に施術可能か'
+      t.boolean :on_shift_10_to_11, default: false, nil: false, comment: '10時~11時に施術可能か'
+      t.boolean :on_shift_11_to_12, default: false, nil: false, comment: '11時~12時に施術可能か'
+      t.boolean :on_shift_12_to_13, default: false, nil: false, comment: '12時~13時に施術可能か'
+      t.boolean :on_shift_13_to_14, default: false, nil: false, comment: '13時~14時に施術可能か'
+      t.boolean :on_shift_14_to_15, default: false, nil: false, comment: '14時~15時に施術可能か'
+      t.boolean :on_shift_15_to_16, default: false, nil: false, comment: '15時~16時に施術可能か'
+      t.boolean :on_shift_16_to_17, default: false, nil: false, comment: '16時~17時に施術可能か'
+      t.boolean :on_shift_17_to_18, default: false, nil: false, comment: '17時~18時に施術可能か'
+      t.boolean :on_shift_18_to_19, default: false, nil: false, comment: '18時~19時に施術可能か'
+      t.boolean :on_shift_19_to_20, default: false, nil: false, comment: '19時~20時に施術可能か'
 
       t.timestamps
     end
