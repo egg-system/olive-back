ad89b3c938321c71dbd1da66aaeeb85c91ec9687
shunyokota <shunyokota@gmail.com>
2019-04-28 15:54:36 +0900

    不要なカラム削除

--- src/db/migrate/20190421023225_create_customer_coupon_tickets.rb
+++ src/db/migrate/20190421023225_create_customer_coupon_tickets.rb
@@ -1,7 +1,6 @@
 class CreateCustomerCouponTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :customer_coupon_tickets do |t|
-      t.references :customer, foreign_key: true
       t.references :customer_coupon, foreign_key: true
 
       t.date :used_at
