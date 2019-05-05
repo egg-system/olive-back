fde4b525d5f35f2397f1202158842990db9c5608
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-05-06 06:38:24 +0900

    fix : インデントの統一

--- src/app/models/menu_category.rb
+++ src/app/models/menu_category.rb
@@ -1,13 +1,13 @@
 class MenuCategory < ApplicationRecord
-    belongs_to :department
+  belongs_to :department
 
-    ACUPUNTURE_CATEGORY_ID = 2
-      
-    scope :join_department, ->{
-       left_joins(:department).select("departments.name as department_name")
-    }
+  ACUPUNTURE_CATEGORY_ID = 2
+    
+  scope :join_department, ->{
+    left_joins(:department).select("departments.name as department_name")
+  }
 
-    scope :join_tables, ->{
-      select('menu_categories.*').join_department
-    }
+  scope :join_tables, ->{
+    select('menu_categories.*').join_department
+  }
 end
--- src/app/models/reservation_detail.rb
+++ src/app/models/reservation_detail.rb
@@ -1,10 +1,10 @@
 class ReservationDetail < ApplicationRecord
-    belongs_to :reservation
-    belongs_to :menu
-    
-    has_many :reservation_detail_options
-    has_many :options, through: :reservation_detail_options
-    accepts_nested_attributes_for :reservation_detail_options, allow_destroy: true
+  belongs_to :reservation
+  belongs_to :menu
+  
+  has_many :reservation_detail_options
+  has_many :options, through: :reservation_detail_options
+  accepts_nested_attributes_for :reservation_detail_options, allow_destroy: true
 
-    validates :menu_id, presence: true
+  validates :menu_id, presence: true
 end
