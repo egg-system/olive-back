3ccc9319bd6e985198d3ae7fe793c42bbb91318d
shunyokota <shunyokota@gmail.com>
2019-05-06 10:17:13 +0900

    何点か修正

--- src/app/controllers/options_controller.rb
+++ src/app/controllers/options_controller.rb
@@ -1,5 +1,6 @@
 class OptionsController < ApplicationController
-  before_action :set_option, only: [:show, :edit, :update, :destroy]
+  before_action :set_option, only: [:show, :update, :destroy]
+  before_action :set_master, only: [:show, :new, :update]
 
   # GET /options
   # GET /options.json
@@ -12,21 +13,9 @@ class OptionsController < ApplicationController
   def show
   end
 
-  def show
-    @option = Option.find(params[:id])
-    @departments = Department.all
-    @skills = Skill.all
-  end
-
   # GET /options/new
   def new
     @option = Option.new
-    @departments = Department.all
-    @skills = Skill.all
-  end
-
-  # GET /options/1/edit
-  def edit
   end
 
   # POST /options
@@ -62,10 +51,15 @@ class OptionsController < ApplicationController
   # DELETE /options/1
   # DELETE /options/1.json
   def destroy
-    @option.destroy
-    respond_to do |format|
-      format.html { redirect_to options_url, notice: 'Option was successfully destroyed.' }
-      format.json { head :no_content }
+    @option.reservation_details
+    if 0 < @option.reservations.count then
+      redirect_to option_url(@option.id), notice: 'すでに利用されているため、削除できません'
+    else
+      @option.destroy
+      respond_to do |format|
+        format.html { redirect_to options_url, notice: 'Option was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
@@ -73,6 +67,11 @@ class OptionsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_option
       @option = Option.find(params[:id])
+    end
+
+    def set_master
+      @departments = Department.all
+      @skills = Skill.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
