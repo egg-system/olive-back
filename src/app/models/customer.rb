213c3adef107e3a13836c41da0fe14c3616fb9ac
shunyokota <shunyokota@gmail.com>
2019-05-18 09:03:16 +0900

    顧客新規登録で、"メールアドレスではありません"のエラーメッセージを差し替える処理

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
-         
+
   include DeviseTokenAuth::Concerns::User
 
   belongs_to :first_visit_store, optional: true, class_name: 'Store', foreign_key: 'first_visit_store_id'
@@ -18,9 +18,14 @@ class Customer < ApplicationRecord
 
   has_many :reservations
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+
   before_validation :sync_none_uid
 
   after_commit :send_register_mail, on: :create
+
+  after_validation :modify_errors
 
   #left join
   scope :join_size, ->{
@@ -34,7 +39,7 @@ class Customer < ApplicationRecord
   scope :like_name, ->(name){
     where("concat(last_name, first_name) like ?", "%#{name}%")
   }
-  
+
   attr_accessor :age, :should_send_mail
 
   after_initialize do
@@ -59,4 +64,16 @@ class Customer < ApplicationRecord
 
     CustomerMailer.confirm_register(self).deliver_now
   end
+
+  def modify_errors
+    #emailのフォーマット不正があった場合に"はメールアドレスではありません"のメッセージが出てくるのを
+    #さしかえる処理
+    isEmailFormatError = self.errors.details[:email].find {|email_error_detail|
+        email_error_detail[:error] == :invalid
+    }.count
+    if isEmailFormatError then
+        self.errors.messages[:email] = [I18n.t('.invalid')]
+    end
+  end
 end
+
