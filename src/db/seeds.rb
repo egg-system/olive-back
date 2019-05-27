4179671aa3150b799ae3b9e1e83b9d83c3feb0a4
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-05-27 20:35:15 +0900

    fix : テストシーダーが必ず動くバグ修正

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -8,7 +8,7 @@ end
 # テストデータ用のseeder 本番でも実行させたい場合は環境変数を渡して、実行する
 doSeed = ENV.fetch('PREVENT_SEED_TEST_DATA', 1) === 0
 ActiveRecord::Base.transaction do
-  unless Rails.env.production? && doSeed
+  if Rails.env.development? || doSeed
     Dir.glob(File.join(Rails.root, 'db', 'seeds', 'test', '*.rb')) do |file|
       load(file)
     end
