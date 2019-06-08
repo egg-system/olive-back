2778a02143a0738ae64a8664b8643f72447f17e8
shunyokota <shunyokota@gmail.com>
2019-06-08 16:44:46 +0900

    会員登録

--- src/app/mailers/customer_mailer.rb
+++ src/app/mailers/customer_mailer.rb
@@ -2,6 +2,7 @@ class CustomerMailer < ApplicationMailer
   def confirm_register(customer)
     @customer = customer
     @store = customer.first_visit_store
+    @store = Store.find(1) if @store.blank? #仮：予約を経由せず会員登録するとstoreがとれないようなので
 
     mail(subject: "#{@store.name} ユーザー登録完了のお知らせ", to: @customer.email) do |format|
       format.text
