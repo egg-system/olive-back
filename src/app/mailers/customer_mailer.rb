88ad99ea6030926e91ea1e320503babf93e8575d
shunyokota <shunyokota@gmail.com>
2019-06-08 17:32:56 +0900

    会員登録　店舗に依存しないつくりに修正

--- src/app/mailers/customer_mailer.rb
+++ src/app/mailers/customer_mailer.rb
@@ -1,10 +1,8 @@
 class CustomerMailer < ApplicationMailer
   def confirm_register(customer)
     @customer = customer
-    @store = customer.first_visit_store
-    @store = Store.find(1) if @store.blank? #仮：予約を経由せず会員登録するとstoreがとれないようなので
 
-    mail(subject: "#{@store.name} ユーザー登録完了のお知らせ", to: @customer.email) do |format|
+    mail(subject: "#{Settings.brand_name} ユーザー登録完了のお知らせ", to: @customer.email) do |format|
       format.text
     end
   end
