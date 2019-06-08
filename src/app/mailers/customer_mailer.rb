2913f686327933fffa099c0112e11a338293b044
shunyokota <shunyokota@gmail.com>
2019-06-08 18:26:06 +0900

    パスワードリセット対応

--- src/app/mailers/customer_mailer.rb
+++ src/app/mailers/customer_mailer.rb
@@ -2,7 +2,7 @@ class CustomerMailer < ApplicationMailer
   def confirm_register(customer)
     @customer = customer
 
-    mail(subject: "#{Settings.brand_name} ユーザー登録完了のお知らせ", to: @customer.email) do |format|
+    mail(subject: "#{Settings.brand.name} ユーザー登録完了のお知らせ", to: @customer.email) do |format|
       format.text
     end
   end
