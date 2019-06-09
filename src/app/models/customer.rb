382e73b2db5ae3604b8027b76cc3cc54c0e701b8
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-06-09 16:30:02 +0900

    fix : 更新時のバリデーションを追加

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -30,7 +30,7 @@ class Customer < ApplicationRecord
   before_validation :sync_none_uid
 
   validates :tel, numericality: true
-  validates :password, presence: true, if: :member?
+  validates :password, presence: true, on: :create, if: :member?
 
   #left join
   scope :join_size, ->{
