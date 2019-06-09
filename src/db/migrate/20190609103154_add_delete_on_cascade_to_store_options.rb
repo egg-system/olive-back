2b29bb88f15be92ae0b15b7ca7de68183f83ad7a
shunyokota <shunyokota@gmail.com>
2019-06-09 19:52:08 +0900

    【オプション】インデプスを削除できない（削除しようとすると落ちる）

---
+++ src/db/migrate/20190609103154_add_delete_on_cascade_to_store_options.rb
@@ -0,0 +1,15 @@
+class AddDeleteOnCascadeToStoreOptions < ActiveRecord::Migration[5.2]
+  def up
+    remove_foreign_key :store_options, :options
+    add_foreign_key :store_options, :options, { on_delete: :cascade }
+    remove_foreign_key :store_options, :stores
+    add_foreign_key :store_options, :stores, { on_delete: :cascade }
+  end
+
+  def down
+    remove_foreign_key :store_options, :options
+    add_foreign_key :store_options, :options
+    remove_foreign_key :store_options, :stores
+    add_foreign_key :store_options, :stores
+  end
+end
