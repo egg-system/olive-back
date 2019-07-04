03c186242aa732462419b46d9d55856728cd0248
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-07-04 22:28:01 +0900

    fix : auditedの設定追加

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
+  audited except: :tokens
 
   include DeviseTokenAuth::Concerns::User
   include PaginationModule
