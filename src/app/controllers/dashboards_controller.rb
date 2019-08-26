d0eb0741ddde3876c747bd810aad412052439883
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-08-26 15:46:43 +0900

    fix : 店舗ごとに初期表示をわける

--- src/app/controllers/dashboards_controller.rb
+++ src/app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
     @reservations = Reservation.where(reservation_date: Date.today)
+      .where(store_id: viewable_stores.pluck(:id))
   end
 end
