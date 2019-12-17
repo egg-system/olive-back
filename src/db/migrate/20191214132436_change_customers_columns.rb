ef84a148d1f88584472adcd56531e0d933ec9c8c
haji33 <hajime.sasanuma.0828@gmail.com>
2019-12-17 23:29:14 +0900

    メールアドレスを修正

--- src/db/migrate/20191214132436_change_customers_columns.rb
+++ src/db/migrate/20191214132436_change_customers_columns.rb
@@ -1,9 +1,9 @@
 class ChangeCustomersColumns < ActiveRecord::Migration[5.2]
   def change
-    change_column :customers, :email, :string, default: "test@test.com"
+    change_column :customers, :email, :string, default: "common.mail@olivebodycare.healthcare"
 
     Customer.all.each { |customer|
-      customer.update_attributes(email: "test@test.com") if !customer.email.present?
+      customer.update_attributes(email: "common.mail@olivebodycare.healthcare") if !customer.email.present?
     }
   end
 end
