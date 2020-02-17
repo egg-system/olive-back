e8315502be39fe1cbab28581ee0acde42d41f305
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-02-18 02:04:55 +0900

    fix : すべての重複が表示されるように修正

--- src/app/helpers/customers_helper.rb
+++ src/app/helpers/customers_helper.rb
@@ -1,20 +1,31 @@
 module CustomersHelper
   def duplicated_customer_alerts
-    return Settings.duplicated_customer.to_h.values.map do |item|
-      if Customer.group_duplicated(item.columns.map(&:to_s)).exists?
-        { message: item.name, columns: item.columns.join(',') }
+    return Settings.duplicated_customer.to_h.values.map { |item|
+      if Customer.group_duplicated(item.columns).exists?
+        { message: item.name, columns: item.columns }
       end
-    end.select(&:present?)
+    }.select(&:present?)
   end
 
-  def duplicated_customer_views(columns, customers)
-    columns_str = columns.present? ? columns : ''
-    if columns_str === Settings.duplicated_customer.kana.columns.join(',')
+  def duplicated_customer_views(customers)
+    # 重複していない場合、全てのfull_kana or telはバラバラになるため、count > 1になる
+    duplicated_kana = customers.map(&:full_kana).uniq.count === 1
+    duplicated_tel = customers.map(&:tel).uniq.count === 1
+
+    if duplicated_kana && duplicated_tel
+      return "カナの<span class='text-danger'>
+          #{customers.first.full_kana}
+        </span>と、電話番号の<span class='text-danger'>
+          #{customers.first.tel}
+        </span>が重複".html_safe
+    end
+
+    if duplicated_kana
       return "カナの<span class='text-danger'>#{customers.first.full_kana}</span>が重複".html_safe
-    elsif columns_str === Settings.duplicated_customer.tel.columns.join(',')
+    end
+
+    if duplicated_tel
       return "電話番号の<span class='text-danger'>#{customers.first.tel}</span>が重複".html_safe
-    elsif columns_str === Settings.duplicated_customer.kana_tel.columns.join(',')
-      return "カナの<span class='text-danger'>#{customers.first.full_kana}</span>と、電話番号の<span class='text-danger'>#{customers.first.tel}</span>が重複".html_safe
     end
   end
 end
