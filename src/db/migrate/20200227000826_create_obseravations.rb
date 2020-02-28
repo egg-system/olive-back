888dcd5e0e2d315f6d77334a862e37f1b7b49e2c
haji33 <hajime.sasanuma.0828@gmail.com>
2020-02-28 09:25:23 +0900

    テーブルの項目を修正

--- src/db/migrate/20200227000826_create_obseravations.rb
+++ src/db/migrate/20200227000826_create_obseravations.rb
@@ -2,14 +2,17 @@ class CreateObseravations < ActiveRecord::Migration[5.2]
   def change
     create_table :obseravations do |t|
       t.references :store, foreign_key: { on_delete: :cascade }, comment: '店舗ID'
-      t.date :visit_date, comment: '来院日時'
+      t.date :visit_date, comment: '来院日'
+      t.integer :operation_time, comment: '施術時間（分）'
       t.references :staff, foreign_key: { on_delete: :cascade }, comment: '対応したスタッフID'
-      t.references :menu, foreign_key: { on_delete: :nullify }, comment: 'メニューID'
-      t.references :option, foreign_key: { on_delete: :nullify }, comment: 'オプションID'
-      t.string :merchandise, comment: '商品'
+      t.references :menu, foreign_key: { on_delete: :nullify }, comment: 'メニューID ※ square連携後に廃止'
+      t.string :option_ids, comment: '複数のオプションIDをカンマ区切りで文字列にしたもの ※ square連携後に廃止'
+      t.string :merchandise, comment: '商品 ※ square連携後に廃止'
       t.text :observation_history, comment: '経過履歴'
-      t.integer :coupon_count, comment: '回数券残'
-      t.integer :op_coupon_count, comment: 'OP回数券残'
+      t.string :coupon_name, comment: '回数券名称 ※ square連携後に廃止'
+      t.integer :coupon_count, comment: '回数券残 ※ square連携後に廃止'
+      t.string :op_coupon_name, comment: 'OP回数券名称 ※ square連携後に廃止'
+      t.integer :op_coupon_count, comment: 'OP回数券残 ※ square連携後に廃止'
 
       t.timestamps
     end
---
+++ src/db/migrate/20200227235701_add_total_sales_to_customers.rb
@@ -0,0 +1,9 @@
+class AddTotalSalesToCustomers < ActiveRecord::Migration[5.2]
+  def up
+    add_column :customers, :total_sales, :integer, default: 0, comment: 'FMに登録されている売上の合計額を移行した項目。新規ユーザーは0になる見込み'
+  end
+
+  def down
+    remove_column :customers, :total_sales, :integer
+  end
+end
