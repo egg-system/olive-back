14259b473588ca394c06f881347daf02557a5af7
haji33 <hajime.sasanuma.0828@gmail.com>
2020-02-29 21:57:44 +0900

    couponテーブルは使っていないようなので修正

--- src/db/migrate/20200227000826_create_obseravations.rb
+++ src/db/migrate/20200227000826_create_obseravations.rb
@@ -8,9 +8,7 @@ class CreateObseravations < ActiveRecord::Migration[5.2]
       t.string :option_ids, comment: '複数のオプションIDをカンマ区切りで文字列にしたもの ※ square連携後に廃止'
       t.text :merchandise, comment: '商品 ※ square連携後に廃止'
       t.text :observation_history, comment: '経過履歴'
-      t.references :coupon, foreign_key: { to_table: :coupons, on_delete: :nullify }, comment: '回数券ID'
       t.integer :coupon_count, comment: '回数券残'
-      t.references :op_coupon, foreign_key: { to_table: :op_coupon, on_delete: :nullify }, comment: 'OP回数券ID'
       t.integer :op_coupon_count, comment: 'OP回数券残'
 
       t.timestamps
