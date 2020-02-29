46112e63e09093370a9864e25fe363ba15e83b26
haji33 <hajime.sasanuma.0828@gmail.com>
2020-02-29 21:43:27 +0900

    回数券の項目などレビュー修正

--- src/db/migrate/20200227000826_create_obseravations.rb
+++ src/db/migrate/20200227000826_create_obseravations.rb
@@ -2,17 +2,16 @@ class CreateObseravations < ActiveRecord::Migration[5.2]
   def change
     create_table :obseravations do |t|
       t.references :store, foreign_key: { on_delete: :cascade }, comment: '店舗ID'
-      t.date :visit_date, comment: '来院日'
-      t.integer :operation_time, comment: '施術時間（分）'
+      t.datetime :visit_datetime, comment: '来院日時（開始時刻）'
       t.references :staff, foreign_key: { on_delete: :cascade }, comment: '対応したスタッフID'
       t.references :menu, foreign_key: { on_delete: :nullify }, comment: 'メニューID ※ square連携後に廃止'
       t.string :option_ids, comment: '複数のオプションIDをカンマ区切りで文字列にしたもの ※ square連携後に廃止'
-      t.string :merchandise, comment: '商品 ※ square連携後に廃止'
+      t.text :merchandise, comment: '商品 ※ square連携後に廃止'
       t.text :observation_history, comment: '経過履歴'
-      t.string :coupon_name, comment: '回数券名称 ※ square連携後に廃止'
-      t.integer :coupon_count, comment: '回数券残 ※ square連携後に廃止'
-      t.string :op_coupon_name, comment: 'OP回数券名称 ※ square連携後に廃止'
-      t.integer :op_coupon_count, comment: 'OP回数券残 ※ square連携後に廃止'
+      t.references :coupon, foreign_key: { to_table: :coupons, on_delete: :nullify }, comment: '回数券ID'
+      t.integer :coupon_count, comment: '回数券残'
+      t.references :op_coupon, foreign_key: { to_table: :op_coupon, on_delete: :nullify }, comment: 'OP回数券ID'
+      t.integer :op_coupon_count, comment: 'OP回数券残'
 
       t.timestamps
     end
