4c2874e84365b1fe5b0f840e5f8274cc3ba032f8
shunyokota <shunyokota@gmail.com>
2020-03-14 18:52:54 +0900

    区切り件数変更

--- src/app/controllers/customers/duplicate_controller.rb
+++ src/app/controllers/customers/duplicate_controller.rb
@@ -1,5 +1,5 @@
 class Customers::DuplicateController < ApplicationController
-  MAX_DISPLAYED_CUSTOMERS_COUNT = 50
+  MAX_DISPLAYED_CUSTOMERS_COUNT = 20
 
   def index
     return if search_params[:columns].nil?
