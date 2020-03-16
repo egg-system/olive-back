5abf9ad8a56fae5f01a027af6f6a18d0f863f813
gakumac <gaku.machida@gmail.com>
2020-03-16 13:11:44 +0900

    showテンプレートを追加

--- src/app/controllers/medical_records_controller.rb
+++ src/app/controllers/medical_records_controller.rb
@@ -2,10 +2,17 @@ class MedicalRecordsController < ApplicationController
   def index
   end
 
+  # GET /medical_records/1
   def show
+    @customer = Customer.find(params[:id])
+                      # MedicalRecord.find(customer_id: @customer.id)
+    @medical_record = @custmer.medical_records
   end
 
   def new
+    @customer = Customer.find(params[:id])
+                      # MedicalRecord.new(customer_id: @customer.id)
+    @medical_record = @customer.medical_records.build
   end
 
   def edit
