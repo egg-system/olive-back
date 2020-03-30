07a40ec0e3a5588084b24881db3aa25c92426fec
shunyokota <shunyokota@gmail.com>
2020-03-30 21:05:55 +0900

    キャンセルメールが送信できない場合はdisableにして、注意文言を表示する

--- src/app/helpers/reservations_helper.rb
+++ src/app/helpers/reservations_helper.rb
@@ -31,4 +31,14 @@ module ReservationsHelper
     date = audit.created_at.strftime('%Y/%m/%d %H:%M')
     return "#{user_type}が、#{date}に登録"
   end
+
+  def cancel_confirm_message(reservation)
+    message = '本当にキャンセルしますか？'
+    return message unless reservation.email_present?
+    return message + "\nまた、キャンセルメールを送信有無を再度ご確認ください。"
+  end
+
+  def check_box_tag_send_cancel_mail(name, reservation)
+    check_box_tag(name, true, reservation.email_present?, disabled: !reservation.email_present?)
+  end
 end
