24622652902f8eab62590d6181e3a58cb3674edb
gakumac <gaku.machida@gmail.com>
2020-04-10 11:14:11 +0900

    不要なファイルの削除

--- src/test/models/many_posture_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ManyPostureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- src/test/models/medical_record_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MedicalRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
