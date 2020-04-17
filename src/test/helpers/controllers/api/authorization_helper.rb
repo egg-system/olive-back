6a9303ee70ff72c12cc3143c026630a869480ec4
haji33 <hajime.sasanuma.0828@gmail.com>
2020-04-17 09:36:05 +0900

    APIのreservationsのテスト実装

---
+++ src/test/helpers/controllers/api/authorization_helper.rb
@@ -0,0 +1,19 @@
+module AuthorizationHelper
+  def sign_up(user)
+    # The argument 'user' should be a hash that includes the params 'email' and 'password'.
+    post '/auth/',
+      params: { email: user[:email],
+                password: user[:password],
+                password_confirmation: user[:password] },
+      as: :json
+  end
+
+  def auth_tokens_for_user(user)
+    # The argument 'user' should be a hash that includes the params 'email' and 'password'.
+    post '/api/customers/sign_in/',
+      params: { email: user[:email], password: user[:password] },
+      as: :json
+    # The three categories below are the ones you need as authentication headers.
+    response.headers.slice('client', 'access-token', 'uid')
+  end
+end
