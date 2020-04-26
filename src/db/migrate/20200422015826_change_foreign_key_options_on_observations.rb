d4910f0d4666efa21adcaea903264fa5291b656f
shunyokota <shunyokota@gmail.com>
2020-04-26 13:01:12 +0900

    コメント追記

--- src/db/migrate/20200422015826_change_foreign_key_options_on_observations.rb
+++ src/db/migrate/20200422015826_change_foreign_key_options_on_observations.rb
@@ -1,5 +1,6 @@
 class ChangeForeignKeyOptionsOnObservations < ActiveRecord::Migration[5.2]
   def change
+    #店舗、スタッフ削除時に経過記録まで削除する必要はないので、on deleteの設定をnullifyに修正。
     remove_foreign_key :observations, :stores
     add_foreign_key :observations, :stores, on_delete: :nullify
 
