5ec696204c5f0ab3796a57a05a4093e1d225ddcd
haji33 <hajime.sasanuma.0828@gmail.com>
2020-04-29 11:18:27 +0900

    Lint/UnusedMethodArgument

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from Exception, with: :render_500 if Rails.env.production?
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     new_staff_session_path
   end
 
