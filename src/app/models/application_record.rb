0ef7f2228784b3e791820a86526210e221a89c85
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-09-24 09:23:59 +0900

    feat : クライアントコードの追加

--- src/app/models/application_record.rb
+++ src/app/models/application_record.rb
@@ -1,9 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
+  require 'square'
+
   self.abstract_class = true
 
   protected
 
   def reserve_url
     return "https://#{Settings.domain.reserve}"
+  end
+
+  def square_client
+    return Square::Client.new(
+      access_token: ENV['SQUARE_API_TOKEN'],
+      environment: Rails.env.production? ? 'production' : 'sandbox'
+    )
   end
 end
