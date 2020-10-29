e61e053727988f4900bd3d3da2b2a3b97e642661
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-10-29 17:09:23 +0900

    feat : 不要なrequireの削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const glob = require('glob');
 const webpack = require('webpack');
 const ManifestPlugin = require('webpack-manifest-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
