20ddc9fa56555db0dacbe63f193805a59b9a52aa
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-11-23 22:01:29 +0900

    feat : 描画処理の実装

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,4 +1,8 @@
 import { renderCalendar } from './render/calendar'
+import { renderShiftCheckboxes } from './render/shifts-checkboxes'
 
 // カレンダーの描画処理
 renderCalendar()
+
+// シフト用のチェックボックスの描画処理
+renderShiftCheckboxes()
