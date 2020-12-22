fef06212ab92781e463bb8344618ac484741b61c
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-12-22 23:32:53 +0900

    feat : 検索処理の実装

---
+++ src/app/controllers/api/admin/reservations_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  module Admin
+    class ReservationsController < ApplicationController
+      def index
+        start_date = Date.parse(search_params[:start_date])
+        end_date = Date.parse(search_params[:end_date])
+
+        reservations = Reservation
+          .where(store_id: search_params[:store_id])
+          .where(reservation_date: start_date..end_date)
+          .where(canceled_at: nil)
+          .eager_load(:staff)
+          .eager_load(:shifts)
+
+        reservations_json = reservations.map do |reservation|
+          {
+            id: reservation.id,
+            date: reservation.reservation_date,
+            startTime: reservation.start_time.to_s,
+            endTime: reservation.end_time.to_s,
+            customer: {
+              id: reservation.customer.id,
+              name: reservation.customer.full_name,
+            },
+            staff: {
+              id: reservation.staff.id,
+              name: reservation.staff.full_name,
+            },
+            shiftIds: reservation.shifts.pluck(:id)
+          }
+        end
+
+        render json: reservations_json   
+      end
+
+      protected
+
+      def search_params
+        return params.permit(:store_id, :start_date, :end_date)
+      end
+    end
+  end
+end
---
+++ src/app/controllers/api/admin/shifts_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module Admin
+    class ShiftsController < ApplicationController
+      def index
+        start_date = Date.parse(search_params[:start_date])
+        end_date = Date.parse(search_params[:end_date])
+
+        shifts = Shift
+          .where(store_id: search_params[:store_id])
+          .where(date: start_date..end_date)
+          .eager_load(:staff)
+          .eager_load(:reservation)
+
+        shifts_json = shifts.map do |shift|
+          reservation_id = shift.reservation.nil? ? nil : shift.reservation.id
+          {
+            id: shift.id,
+            date: shift.date,
+            startTime: shift.start_at.to_s,
+            endTime: shift.end_at.to_s,
+            reservationId: reservation_id,
+            staff: {
+              id: shift.staff.id,
+              name: shift.staff.full_name,
+            }
+          }
+        end
+
+        render json: shifts_json   
+      end
+
+      protected
+
+      def convert_shift_reservation(shift)
+        unless shift.reservation.nil?
+          
+        end
+      end
+
+      def search_params
+        return params.permit(:store_id, :start_date, :end_date)
+      end
+    end
+  end
+end
