6438d424a3108e8ecf6a76e2a452a513620a1db1
nuno929 <t-mark5@kce.biglobe.ne.jp>
2021-01-06 18:05:55 +0900

    feat : 各種詳細なコンポーネントの実装

---
+++ frontend/class/ReservationCalendar/Calendar/CalendarCell.tsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import moment, { Moment } from 'moment'
+import { sortBy, groupBy } from 'lodash'
+
+import styles from '../../../styles/ReservationCalendar.scss'
+import CalenderTime, { Reservation, Shift } from './CalendarTime'
+
+export type { Reservation, Shift }
+
+interface Props {
+  date: Moment
+  inStandardMonth: Boolean
+  reservations: Reservation[]
+  shifts: Shift[]
+}
+
+export default class CalendarCell extends Component<Props> {
+  protected formatedDate() {
+    const format = this.props.inStandardMonth ? 'D' : 'M/D'
+    return this.props.date.format(format)
+  }
+
+  protected rendarTimeRows() {
+    // 時間帯でシフトをまとめる
+    const shiftTimes = groupBy(this.props.shifts, (shift) => {
+      const startTime = moment(shift.startTime as string, 'HH:mm:ss')
+      return startTime.hour()
+    })
+
+    // 連想配列にすると、キーが文字列になるので、数値に変換してソートする
+    const sortedShiftTimes = sortBy(Object.keys(shiftTimes), Number)
+    return sortedShiftTimes.map((shiftTime) => {
+      const reservations = this.props.reservations.filter((reservation) => {
+        const reservedTime = moment(reservation.startTime as string, 'HH:mm:ss')
+        return reservedTime.hour() === Number(shiftTime)
+      })
+
+      return <CalenderTime
+        key={ `${this.props.date.unix()}-${shiftTime}` }
+        shiftTime={ shiftTime }
+        shifts={ shiftTimes[shiftTime] }
+        reservations={ reservations }
+      />
+    })
+  }
+
+  render() {
+    return <div className={ styles.calendarCell }>
+      <div className={ styles.calendarCellDate }>
+        { this.formatedDate() }
+      </div>
+      <div className={ styles.calendarTimeRows }>
+        { this.rendarTimeRows() }
+      </div>
+    </div>
+  }
+}
