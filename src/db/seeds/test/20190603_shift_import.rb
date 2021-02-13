a45f8f25a4e65401b4313f7c21082228e148c864
natsume0718 <moto718program@gmail.com>
2021-02-14 02:14:52 +0900

    update seed

--- src/db/seeds/test/20190603_shift_import.rb
+++ src/db/seeds/test/20190603_shift_import.rb
@@ -1,9 +1,7 @@
 require('csv')
+require('fileutils')
 
-CSV.read(
-  Rails.root.join("storage/test/csv/test_import_shift.csv"),
-  headers: true, encoding:
-  'Shift_JIS:UTF-8'
-).map { |row|
-  Shift.import(row)
-}
+file_name = "test_import_shift.csv"
+FileUtils.mv(Rails.root.join('storage', 'test', 'csv', file_name), Shift.save_csv_path('test_import_shift.csv'))
+
+Shift.import(file_name)
