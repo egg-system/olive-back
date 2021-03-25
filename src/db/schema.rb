f3497149f804c4f2f736a9285c0c3c51270953b3
natsume0718 <moto718program@gmail.com>
2021-03-26 01:38:50 +0900

    update migrate

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_173226) do
+ActiveRecord::Schema.define(version: 2021_03_26_064355) do
 
   create_table "audits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "auditable_id"
