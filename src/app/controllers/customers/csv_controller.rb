46bf54d6ee905888a0cd34c60c74036086a76956
natsume0718 <moto718program@gmail.com>
2021-06-02 00:39:27 +0900

    viewに変更

--- src/app/controllers/customers/csv_controller.rb
+++ src/app/controllers/customers/csv_controller.rb
@@ -1,111 +1,22 @@
 class Customers::CsvController < ApplicationController
-  require 'csv'
-
-  HEADERS = [
-    "顧客ID",
-    "FMID",
-    "姓",
-    "名",
-    "セイ",
-    "メイ",
-    "電話番号",
-    "固定電話番号",
-    "メールアドレス",
-    "ログインID",
-    "パスワード",
-    "メール受け取り",
-    "初回ご来店店舗",
-    "初回ご来店日",
-    "直近ご来店店舗",
-    "コメント",
-    "郵便番号",
-    "住所",
-    "生年月日",
-    "年齢",
-    "カルテNo",
-    "診察券発行有無",
-    "紹介者",
-    "来店経緯",
-    "最寄駅",
-    "Web検索",
-    "お子様の数",
-    "赤ちゃんの年齢",
-    "職業",
-    "動物占い",
-    "サイズ",
-    "使用状況",
-    "売上総額",
-    "square連携"
-  ]
 
   def index
     filename = 'customers_' + Date.current.strftime("%Y%m%d")
-    send_data generate_customer_data, filename: filename, type: 'csv'
-  end
 
-  private
+    @stores = Store.all
+    @visit_reasons = VisitReason.all
+    @nearest_stations = NearestStation.all
+    @baby_ages = BabyAge.all
+    @occupations = Occupation.all
+    @zoomancies = Zoomancy.all
+    @sizes = Size.all
+    @customers = Customer.all
 
-  def generate_customer_data
-    # 先に取得しておく
-    stores = Store.all
-    visit_reasons = VisitReason.all
-    nearest_stations = NearestStation.all
-    baby_ages = BabyAge.all
-    occupations = Occupation.all
-    zoomancies = Zoomancy.all
-    sizes = Size.all
-
-    bom = '\uFEFF'
-    CSV.generate(bom) do |csv|
-      csv << HEADERS
-      Customer.find_each do |customer|
-        first_visit_store = stores.find { |store| store.id == customer.first_visit_store_id }
-        last_visit_store = stores.find { |store| store.id == customer.last_visit_store_id }
-        visit_reason = visit_reasons.find { |reason| reason.id == customer.visit_reason_id }
-        nearest_station = nearest_stations.find { |station| station.id == customer.nearest_station_id }
-        baby_age = baby_ages.find { |baby_age| baby_age.id == customer.baby_age_id }
-        occupation = occupations.find { |occupation| occupation.id == customer.occupation_id }
-        zoomancy = zoomancies.find { |zoomancy| zoomancy.id == customer.zoomancy_id }
-        size = sizes.find { |size| size.id == customer.size_id }
-        values = [
-          customer.id,
-          customer.fmid,
-          customer.last_name,
-          customer.first_name,
-          customer.last_kana,
-          customer.first_kana,
-          customer.tel,
-          customer.fixed_line_tel,
-          customer.display_email,
-          customer.uid,
-          customer.password,
-          customer.can_receive_mail ? "受け取る" : "受け取らない",
-          first_visit_store&.name,
-          customer.first_visited_at,
-          last_visit_store&.name,
-          customer.comment,
-          customer.zip_code,
-          customer.address,
-          customer.birthday,
-          customer.age,
-          customer.card_number,
-          customer.has_registration_card,
-          customer.introducer,
-          visit_reason&.name,
-          nearest_station&.name,
-          customer.searched_by,
-          customer.children_count,
-          baby_age&.name,
-          occupation&.name,
-          zoomancy&.name,
-          size&.name,
-          customer.is_deleted ? "未使用" : "使用中",
-          customer.fm_total_amount,
-          customer.square_customer_exists? ? "連携済み" : "未連携",
-        ]
-        csv << values
+    respond_to do |format|
+      format.csv do
+        response.headers['Content-Disposition'] = 'attachment; filename="' + filename + '.csv"'
+        render "customers/csv/index.csv.erb"
       end
     end
   end
-
 end
