c02feee0753e6750478f72607be44fe9a72a52ba
kanahebi <miyasaka@noft-ware.com>
2021-06-09 14:47:21 +0900

    来店情報を編集できる権限を修正

--- src/app/models/visit_store_validator.rb
+++ src/app/models/visit_store_validator.rb
@@ -1,5 +1,6 @@
 class VisitStoreValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
+    return if value.nil?
     record.errors.add(:visit_store_ids, :blank) if (value - ['', nil]).blank?
   end
 end
