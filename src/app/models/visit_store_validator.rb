2d79482b29136dbe53a45e7bd410d31e5f254277
natsume0718 <moto718program@gmail.com>
2021-06-10 02:03:45 +0900

    to each

--- src/app/models/visit_store_validator.rb
+++ src/app/models/visit_store_validator.rb
@@ -1,6 +1,6 @@
 class VisitStoreValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
-    return unless (value)
+    return unless value
 
     record.errors.add(:visit_store_ids, :blank) if (value - ['', nil]).blank?
   end
