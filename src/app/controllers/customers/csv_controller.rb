aebac6413d4c57d470d35771d4855ae73d8da277
natsume0718 <moto718program@gmail.com>
2021-08-23 09:40:42 +0900

    remove empty line

--- src/app/controllers/customers/csv_controller.rb
+++ src/app/controllers/customers/csv_controller.rb
@@ -1,13 +1,11 @@
 class Customers::CsvController < ApplicationController
   def index
     filename = 'customers_' + Date.current.strftime("%Y%m%d")
-
     headers.delete("Content-Length")
     headers["Cache-Control"] = "no-cache"
     headers['Content-Type'] = 'text/csv'
     headers['X-Accel-Buffering'] = 'no'
     response.headers['Content-Disposition'] = 'attachment; filename="' + filename + '.csv"'
-
     self.response_body = build_csv
   end
 
