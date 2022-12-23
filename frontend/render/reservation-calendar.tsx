b3cfbf7f30ef030264dc096403192d7480ab09f3
nuno929 <t-mark5@kce.biglobe.ne.jp>
2022-12-23 11:08:50 +0900

    fix : 誤記の修正

--- frontend/render/reservation-calendar.tsx
+++ frontend/render/reservation-calendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReservationCalendaer from '../class/ReservationCalendar/ReservationCalendar'
+import ReservationCalendar from '../class/ReservationCalendar/ReservationCalendar'
 
 export const renderReservationCalendar = () => {
   const elementId = 'react_reservation_calendar'
@@ -12,7 +12,7 @@ export const renderReservationCalendar = () => {
 
   const storeStaffOptions = element.getAttribute('data-store-staffs') as string
   ReactDOM.render(
-    <ReservationCalendaer stores={ JSON.parse(storeStaffOptions) } />,
+    <ReservationCalendar stores={ JSON.parse(storeStaffOptions) } />,
     element
   )
 }
