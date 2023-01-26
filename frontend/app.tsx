3d79fba8118795c5454f5c361cfac19d25f2c105
nuno929 <t-mark5@kce.biglobe.ne.jp>
2023-01-27 08:57:28 +0900

    fix : 並び順の修正

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -14,13 +14,14 @@ renderCalendar()
 // シフト用のチェックボックスの描画処理
 renderShiftCheckboxes()
 
-// シフトの空いているスタッフのみのセレクトボックスの描画処理
-renderAssignableStaffSelect()
-
 // 予約一覧のカレンダー表示
 renderReservationCalendar()
 
 // ダウンロードリンク
 renderDownloadButton()
 
+// シフトの空いているスタッフのみのセレクトボックスの描画処理
+renderAssignableStaffSelect()
+
+// メニュー・オプションの描画
 renderInputMenuOptions()
